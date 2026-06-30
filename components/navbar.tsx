7ca582ad78e53df6e0dc688ecba62f3a5420d903
BabaYagashka <virajshinde2005@gmail.com>
2026-06-30 13:26:46 +0530

    feat: enhance navbar with user session handling and dropdown menu for authenticated users

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,22 @@
+"use client";
+
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { getSession } from "@/lib/auth/auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import SignOutBtn from "./sign-out-btn";
+import { useSession } from "@/lib/auth/auth-client";
 
 export default function Navbar() {
+  const { data: session } = useSession();
   return (
     <nav className="border-b border-gray-200 bg-white">
       <div className="container flex h-16 w-full items-center px-4">
@@ -15,12 +29,48 @@ export default function Navbar() {
         </Link>
 
         <div className="ml-auto flex items-center gap-4">
-          <Link href="/sign-in">
-            <Button variant="ghost">Log In</Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button variant="default">Start for Free</Button>
-          </Link>
+          {session?.user ? (
+            <>
+              <Link href="/dashboard">
+                <Button variant="ghost">Dashboard</Button>
+              </Link>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant="ghost" className="w-8 h-8 rounded-full p-0">
+                    <Avatar>
+                      <AvatarFallback className="bg-primary text-primary-foreground">
+                        {session.user.name?.charAt(0).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent className="w-56" align="end">
+                  <DropdownMenuLabel>
+                    <div className="flex flex-col space-y-1 text-foreground">
+                      <p className="text-sm font-medium leading-none">
+                        {session.user.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {session.user.email}
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <SignOutBtn />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in">
+                <Button variant="ghost">Log In</Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button variant="default">Start for Free</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
